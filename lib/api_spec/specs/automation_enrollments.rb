4e4998fcccfec7de413638bef3eb14ada62955c8
Ayelen Sanchez <ayelensanchez@Ayelens-MacBook-Pro.local>
2022-01-28 15:10:49 -0300

    Fixing endpoints for automation_enrollments

--- lib/api_spec/specs/automation_enrollments.rb
+++ lib/api_spec/specs/automation_enrollments.rb
@@ -17,7 +17,7 @@ class ApiSpec::Spec
     automation_enrollments.method('Create') do |method|
       method.synopsis = 'Creates an automation enrollment with the provided data'
       method.http_method = 'POST'
-      method.uri = '/v1/automation_enrollments'
+      method.uri = '/v1/automations/:id/automation_enrollments'
 
       method.parameter('body') do |p|
         p.required = 'Y'
@@ -29,7 +29,7 @@ class ApiSpec::Spec
     automation_enrollments.method('Destroy') do |method|
       method.synopsis = 'Destroy a specific Automation Enrollment based on the signup'
       method.http_method = 'DELETE'
-      method.uri = '/v1/automations/:id/automation_enrollments/'
+      method.uri = '/v1/automations/:id/automation_enrollments'
 
       method.parameter('signup_id') do |p|
         p.required = 'Y'
@@ -37,7 +37,7 @@ class ApiSpec::Spec
         p.description = 'The Signup id of the automation enrollment'
       end
 
-      method.parameter('automation_id') do |p|
+      method.parameter('id') do |p|
         p.required = 'Y'
         p.type = 'int'
         p.description = 'The Automation id of the automation enrollment'
